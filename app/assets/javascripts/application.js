63386489f5917dbc65de422252db122dcd30ea56
Seiji Naganuma <snaganumas@gmail.com>
2016-05-03 21:18:37 -0400

    make the asset compile work

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
 //= require bootstrap-sprockets
+//= require jquery_ujs
+//= require turbolinks
 //= require react
 //= require react_ujs
 //= require components
