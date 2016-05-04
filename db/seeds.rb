2e3fd754e46c3bdc7e79deaf5893fbe1a523ac56
Seiji Naganuma <snaganumas@gmail.com>
2016-05-03 20:59:36 -0400

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Record.create(title: 'First Record', date: Date.today, amount: 500)
+Record.create(title: 'Second Record', date: Date.today, amount: -1000)
+
